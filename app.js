13d3cb645a2fcad78e6d700a1856db91f398709a
humasrt08-gps <humasrt08@gmail.com>
2026-06-01 19:00:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbz9-p9pbbgpssybEiO-ti3MshnwcTExrVRPW4Mkm1tpOkYBRclWVbe3rvq5CNmAwvhb/exec';
 
-async function loadNama() {
+let dataWarga = [];
+
+async function loadData() {
 
     const response = await fetch(API_URL);
     const result = await response.json();
 
-    // Ambil nama unik
+    dataWarga = result.data;
+
     const namaUnik = [...new Set(
-        result.data
-            .map(item => item.Nama)
-            .filter(nama => nama)
-    )].sort();
+        dataWarga.map(item => item.Nama)
+    )];
 
-    const datalist = document.getElementById('listNama');
-
-    datalist.innerHTML = namaUnik
-        .map(nama => `<option value="${nama}">`)
-        .join('');
+    document.getElementById('listNama').innerHTML =
+        namaUnik.map(nama => `<option value="${nama}">`).join('');
 }
 
-loadNama();
+document.getElementById('nama').addEventListener('change', function() {
+
+    const warga = dataWarga.find(
+        item => item.Nama === this.value
+    );
+
+    if (!warga) return;
+
+    document.getElementById('nik').value = warga['No KTP'] || '';
+    document.getElementById('jk').value = warga['Jenis Kelamin'] || '';
+    document.getElementById('tempat').value = warga['Tempat Lahir'] || '';
+    document.getElementById('tgl_lahir').value = warga['Tanggal lahir'] || '';
+    document.getElementById('wn').value = warga.Kewarganegaraan || '';
+    document.getElementById('alamat_ktp').value = warga['Alamat KTP'] || '';
+    document.getElementById('blok').value = warga.Blok || '';
+    document.getElementById('no_rumah').value = warga.Nomor || '';
+});
+
+loadData();
